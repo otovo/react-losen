bd02903c99e0fba1077c4c852554fe506ac3a24d
Rikard Eide <rix1@otovo.com>
2018-01-16 01:15:28 +0100

    wizard: added simple error component to display error messages

---
+++ ErrorMessage.js
@@ -0,0 +1,22 @@
+// @flow
+import * as React from 'react';
+import PropTypes from 'prop-types';
+
+type Props = {
+  className: string,
+};
+type Context = {
+  errorMessage: string,
+};
+
+const ErrorMessage = (props: Props, context: Context) => (
+  <div className={props.className}>
+    <span className="h2">{context.errorMessage}</span>
+  </div>
+);
+
+ErrorMessage.contextTypes = {
+  errorMessage: PropTypes.string.isRequired,
+};
+
+export default ErrorMessage;
