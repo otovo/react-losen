4880c34793fd20fe3c086e8f7827ed4028eeda71
Rikard Eide <rix1@otovo.com>
2018-07-31 01:43:59 +0200

    Jest: Add jest and setup babel for testing

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+module.exports = {
+  // This option sets the URL for the jsdom environment. It is reflected in properties such as location.href
+  testURL: 'http://localhost/',
+  // Indicates whether each individual test should be reported during the run
+  verbose: true,
+};
