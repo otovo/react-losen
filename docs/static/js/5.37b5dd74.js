039296c3fbcc67cb1cdc09ec19b2871f3d7d14ae
Rikard Eide <rix1@otovo.com>
2019-02-15 11:15:35 +0100

    Docz: Build docs
    I tried setting this up with a github action, but with little luck...

---
+++ docs/static/js/5.37b5dd74.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{"./.docz/app/imports.js":function(n,e,i){"use strict";i.r(e),i.d(e,"imports",function(){return p});var p={"_index.mdx":function(){return i.e(3).then(i.bind(null,"./pages/_index.mdx"))},"basicExample.mdx":function(){return Promise.all([i.e(0),i.e(2)]).then(i.bind(null,"./pages/basicExample.mdx"))}}}},0,[0,2,3]]);
+//# sourceMappingURL=5.b3be69553827a5707e5d.js.map
