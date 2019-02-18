906688d6ef8857d5f23d1846d51d8c210018933c
Rikard Eide <rix1@otovo.com>
2019-02-18 14:23:33 +0100

    Pack: Prefer named export instead of default

--- src/index.js
+++ src/index.js
@@ -2,8 +2,4 @@ import Wizard from './Wizard';
 import Step from './Step';
 import Controls from './Controls';
 
-export default {
-  Wizard,
-  Step,
-  Controls,
-};
+export { Wizard, Step, Controls };
